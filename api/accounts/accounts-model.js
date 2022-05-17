fbc1c18bbf3949b4a2a14b536eb5838a60fb3f3c
Ricardorouge <ricky032929@gmail.com>
2022-05-17 15:34:22 -0400

    finished models

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,43 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts')
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts')
+  .where({id:id})
+  .first()
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  db('accounts')
+  .insert(account)
+  .then(result=>{
+    return getById(result)
+  })
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  db('accounts')
+  .update(account)
+  .where({id:id})
+  .then(result=>{
+    return getById(result)
+  })
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  const result = getById(id)
+  db('accounts')
+  .delete()
+  .where({id:id})
+  return result
 }
 
 module.exports = {
